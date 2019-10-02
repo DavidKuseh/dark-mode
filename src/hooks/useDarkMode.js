1c62fd699ababee52ad56ab50ace58f9a7f0e6dd
David Kuseh <davegru203@gmail.com>
2019-10-02 15:12:27 +0000

    imported useDarkMode hook into Navbar, dark mode working

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
-import useLocalStorage from './useLocalStorage';
+import {useLocalStorage} from './useLocalStorage';
 
-export default useDarkMode = (key, initialValue) => {
+export const useDarkMode = (key, initialValue) => {
     const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
 
     useEffect(()=> {
         if (darkMode) {
-            return body.classList.add('dark-mode');
+            return document.body.classList.add('dark-mode');
         }
-        return body.classList.remove('dark-mode');
+        return document.body.classList.remove('dark-mode');
     }, [darkMode]);
 
     return [darkMode, setDarkMode];
